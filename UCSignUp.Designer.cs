796f9939cb3f4018d1421e63bb9e3433f4494883
Nguyễn Nguyên Toàn Khoa <toankhoa86443@gmail.com>
2024-03-06 14:41:39 +0700

    Update project of Khoa

---
+++ UCSignUp.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ProjectFinalWF
+{
+    partial class UCSignUp
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pcbLogoLogin = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblUserNameSignup = new System.Windows.Forms.Label();
+            this.txtUserNameSignUp = new System.Windows.Forms.TextBox();
+            this.txtPasswordSignUp = new System.Windows.Forms.TextBox();
+            this.txtModifySignUp = new System.Windows.Forms.TextBox();
+            this.txtEmailSignUp = new System.Windows.Forms.TextBox();
+            this.lblPasswordSignUp = new System.Windows.Forms.Label();
+            this.lblModifySignUp = new System.Windows.Forms.Label();
+            this.lblEmailSignUp = new System.Windows.Forms.Label();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogoLogin)).BeginInit();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.flowLayoutPanel2.BackgroundImage = global::ProjectFinalWF.Properties.Resources._2c8526e8605d8770d761a3d3d314229a__1_;
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(960, 0);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(304, 513);
+            this.flowLayoutPanel2.TabIndex = 16;
+            // 
+            // pcbLogoLogin
+            // 
+            this.pcbLogoLogin.Image = global::ProjectFinalWF.Properties.Resources.male_icon_54__1_;
+            this.pcbLogoLogin.Location = new System.Drawing.Point(3, 3);
+            this.pcbLogoLogin.Name = "pcbLogoLogin";
+            this.pcbLogoLogin.Size = new System.Drawing.Size(101, 94);
+            this.pcbLogoLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbLogoLogin.TabIndex = 20;
+            this.pcbLogoLogin.TabStop = false;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.pcbLogoLogin);
+            this.flowLayoutPanel3.Controls.Add(this.label1);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(472, 56);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(111, 100);
+            this.flowLayoutPanel3.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "label1";
+            // 
+            // lblUserNameSignup
+            // 
+            this.lblUserNameSignup.AutoSize = true;
+            this.lblUserNameSignup.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserNameSignup.Location = new System.Drawing.Point(337, 211);
+            this.lblUserNameSignup.Name = "lblUserNameSignup";
+            this.lblUserNameSignup.Size = new System.Drawing.Size(92, 23);
+            this.lblUserNameSignup.TabIndex = 22;
+            this.lblUserNameSignup.Text = "UserName";
+            // 
+            // txtUserNameSignUp
+            // 
+            this.txtUserNameSignUp.Location = new System.Drawing.Point(472, 210);
+            this.txtUserNameSignUp.Name = "txtUserNameSignUp";
+            this.txtUserNameSignUp.Size = new System.Drawing.Size(187, 22);
+            this.txtUserNameSignUp.TabIndex = 26;
+            // 
+            // txtPasswordSignUp
+            // 
+            this.txtPasswordSignUp.Location = new System.Drawing.Point(472, 267);
+            this.txtPasswordSignUp.Name = "txtPasswordSignUp";
+            this.txtPasswordSignUp.Size = new System.Drawing.Size(187, 22);
+            this.txtPasswordSignUp.TabIndex = 27;
+            // 
+            // txtModifySignUp
+            // 
+            this.txtModifySignUp.Location = new System.Drawing.Point(472, 325);
+            this.txtModifySignUp.Name = "txtModifySignUp";
+            this.txtModifySignUp.Size = new System.Drawing.Size(187, 22);
+            this.txtModifySignUp.TabIndex = 28;
+            // 
+            // txtEmailSignUp
+            // 
+            this.txtEmailSignUp.Location = new System.Drawing.Point(472, 384);
+            this.txtEmailSignUp.Name = "txtEmailSignUp";
+            this.txtEmailSignUp.Size = new System.Drawing.Size(187, 22);
+            this.txtEmailSignUp.TabIndex = 29;
+            // 
+            // lblPasswordSignUp
+            // 
+            this.lblPasswordSignUp.AutoSize = true;
+            this.lblPasswordSignUp.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasswordSignUp.Location = new System.Drawing.Point(337, 267);
+            this.lblPasswordSignUp.Name = "lblPasswordSignUp";
+            this.lblPasswordSignUp.Size = new System.Drawing.Size(85, 23);
+            this.lblPasswordSignUp.TabIndex = 30;
+            this.lblPasswordSignUp.Text = "Password";
+            // 
+            // lblModifySignUp
+            // 
+            this.lblModifySignUp.AutoSize = true;
+            this.lblModifySignUp.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModifySignUp.Location = new System.Drawing.Point(337, 325);
+            this.lblModifySignUp.Name = "lblModifySignUp";
+            this.lblModifySignUp.Size = new System.Drawing.Size(68, 23);
+            this.lblModifySignUp.TabIndex = 31;
+            this.lblModifySignUp.Text = "Modify";
+            // 
+            // lblEmailSignUp
+            // 
+            this.lblEmailSignUp.AutoSize = true;
+            this.lblEmailSignUp.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailSignUp.Location = new System.Drawing.Point(337, 384);
+            this.lblEmailSignUp.Name = "lblEmailSignUp";
+            this.lblEmailSignUp.Size = new System.Drawing.Size(54, 23);
+            this.lblEmailSignUp.TabIndex = 32;
+            this.lblEmailSignUp.Text = "Email";
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignUp.Location = new System.Drawing.Point(472, 433);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(122, 45);
+            this.btnSignUp.TabIndex = 33;
+            this.btnSignUp.Text = "SignUp";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            // 
+            // UCSignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProjectFinalWF.Properties.Resources._5_dieu_can_biet_khi_thiet_ke_website_du_lich__1_;
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.lblEmailSignUp);
+            this.Controls.Add(this.lblModifySignUp);
+            this.Controls.Add(this.lblPasswordSignUp);
+            this.Controls.Add(this.txtEmailSignUp);
+            this.Controls.Add(this.txtModifySignUp);
+            this.Controls.Add(this.txtPasswordSignUp);
+            this.Controls.Add(this.txtUserNameSignUp);
+            this.Controls.Add(this.lblUserNameSignup);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Name = "UCSignUp";
+            this.Size = new System.Drawing.Size(1264, 513);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogoLogin)).EndInit();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.PictureBox pcbLogoLogin;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblUserNameSignup;
+        private System.Windows.Forms.TextBox txtUserNameSignUp;
+        private System.Windows.Forms.TextBox txtPasswordSignUp;
+        private System.Windows.Forms.TextBox txtModifySignUp;
+        private System.Windows.Forms.TextBox txtEmailSignUp;
+        private System.Windows.Forms.Label lblPasswordSignUp;
+        private System.Windows.Forms.Label lblModifySignUp;
+        private System.Windows.Forms.Label lblEmailSignUp;
+        private System.Windows.Forms.Button btnSignUp;
+    }
+}
